2efd20f725a339e769d4556817649a94c1e2995a
王若愚 <wrymax@qq.com>
2017-01-25 23:50:56 +0800

    add users/show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def experts
     @users = User.experts.includes(:expert_profile).paginate(page: params[:page])
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # 个人中心
+  def dashboard
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :users do
     collection do
       get :experts
+      get :dashboard
     end
   end
 end
