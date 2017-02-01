653323d45370da40cf195250098cf88fe6d6f92b
王若愚 <wrymax@qq.com>
2017-01-31 17:08:13 -0800

    update .white-board style

--- app/models/need.rb
+++ app/models/need.rb
@@ -6,4 +6,8 @@ class Need < ApplicationRecord
   has_many :experts, through: :case_experts, class_name: 'User'
 
   validates :content, presence: true, length: { within: 1..5000 }
+
+  def i18n_status
+    I18n.t("custom.need.status.#{self.status}")
+  end
 end
