12bb6e1ef51ddbfc785f5d84bf154a1c25269dd3
王若愚 <wrymax@qq.com>
2017-02-08 01:00:58 -0800

    add admin: create and edit expert info

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,49 @@
 class Admin::UsersController < Admin::ApplicationController
+  def new
+    @user = User.new
+  end
+
   def index
     @experts = User.experts.latest.paginate(page: params[:page], per_page: 10)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 更新专家信息
+      if @user.expert?
+        @user.expert_profile.update!(expert_profile_params)
+      end
+      redirect_back fallback_location: admin_users_path, notice: "信息已更新"
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save(validate: false)
+      # 更新专家信息
+      @user.create_expert_profile(expert_profile_params)
+      redirect_back fallback_location: admin_users_path, notice: "已添加专家账号"
+    else
+      render :new
+    end
+
+  end
+
+  protected
+  
+  def user_params
+    params[:expert_profile] = params[:user].delete(:expert_profile)
+    params[:user].merge({user_type: 'expert'}).permit!
+  end
+
+  def expert_profile_params
+    params[:expert_profile].permit!
   end
 end
