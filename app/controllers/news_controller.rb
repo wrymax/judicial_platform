18cdc0aa7374be35c7ff2538ac73ee89a705ff08
王若愚 <wrymax@qq.com>
2017-03-03 23:35:50 +0800

    show page for work_dynamic and news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,6 @@
 class NewsController < ApplicationController
+  def show
+    @news = News.find(params[:id])
+    @latest_news = News.latest.paginate(page: 1, per_page: 4)
+  end
 end
--- app/controllers/work_dynamics_controller.rb
+++ app/controllers/work_dynamics_controller.rb
@@ -1,2 +1,6 @@
 class WorkDynamicsController < ApplicationController
+  def show
+    @work_dynamic = WorkDynamic.find(params[:id])
+    @latest_work_dynamics = WorkDynamic.latest.paginate(page: 1, per_page: 4)
+  end
 end
