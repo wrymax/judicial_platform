22375f91205d565e6630e33ea3998255a1482b99
王若愚 <wrymax@qq.com>
2017-06-03 16:35:23 +0800

    fix some new issues

--- app/controllers/first_level_tags_controller.rb
+++ app/controllers/first_level_tags_controller.rb
@@ -2,4 +2,8 @@ class FirstLevelTagsController < ApplicationController
   def index
     @first_level_tags = FirstLevelTag.includes(:tags)
   end
+
+  def show
+    @first_level_tag = FirstLevelTag.find(params[:id])
+  end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
 module HomeHelper
+  def home_tag_link(name)
+    link_to name, first_level_tag_path(FirstLevelTag.find_by_name(name))
+  end
 end
