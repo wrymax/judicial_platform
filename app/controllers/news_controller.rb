bedbfeec64cc4c8e43b2032d577bd8789d3557af
王若愚 <wrymax@qq.com>
2017-06-19 12:25:40 +0800

    add list for work dynamics and news; update logos

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,4 +1,9 @@
 class NewsController < ApplicationController
+  def index
+    @news = News.latest.paginate(page: params[:page], per_page: 10)
+    @title = "新闻资讯"
+  end
+
   def show
     @news = News.find(params[:id])
     @latest_news = News.latest.paginate(page: 1, per_page: 4)
--- app/controllers/work_dynamics_controller.rb
+++ app/controllers/work_dynamics_controller.rb
@@ -1,4 +1,9 @@
 class WorkDynamicsController < ApplicationController
+  def index
+    @work_dynamics = WorkDynamic.latest.paginate(page: params[:page], per_page: 10)
+    @title = "工作动态"
+  end
+
   def show
     @work_dynamic = WorkDynamic.find(params[:id])
     @latest_work_dynamics = WorkDynamic.latest.paginate(page: 1, per_page: 4)
